bbec19f67d006a321ef5c494edf58b5c76e2ec90
Leonid Latyshev <forlemonster@gmail.com>
2020-12-28 13:03:16 +1000

    endpoint monitor

--- GetIP/Controller/ViewController.m
+++ GetIP/Controller/ViewController.m
@@ -7,9 +7,12 @@
 
 #import "ViewController.h"
 #import "Host.h"
+//#import <Network/path.h>
+//#import <Network/endpoint.h>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *textAddresses;
+//@property (strong, nonatomic) nw_path_monitor_t pathMonitor;
 
 @end
 
@@ -17,6 +20,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // TODO: - add tableviewcontroller, where sections have names of interfaces and rows are ipaddresses
     
     Host *host = [Host new];
     NSArray *addresses = [host getIpAddress];
@@ -26,6 +31,16 @@
     for (int i=0; i < [addresses count]; i++) {
         _textAddresses.text = [_textAddresses.text stringByAppendingFormat: @"%@\n", [addresses objectAtIndex:i]];
     }
+    
+    if (@available(iOS 12.0, *)) {
+        
+//        _pathMonitor = nw_path_monitor_create();
+//        nw_endpoint_t endpoint = nw_endpoint_create_host("8.8.8.8", "80");
+        
+    } else {
+        // Fallback on earlier versions
+    }
+    
 
 }
 
